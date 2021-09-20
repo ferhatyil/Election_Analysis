abd6edf900c88296e2c9828a09079b5ca8bf029d
ferhatyil <88639634+ferhatyil@users.noreply.github.com>
2021-09-19 23:55:41 -0700

    Add files via upload

---
+++ Python_practice.py
@@ -0,0 +1,66 @@
+# What is the score?
+score = 9
+
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+    for county in counties:
+        print(county)
+for i in range(len(counties)):
+    print(counties[i])
+
+for county in counties:
+    print(county)
+
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+for county_dict in voting_data:
+    print(county_dict['county'])
+
+for county_dict in voting_data:
+    print(county_dict['registered_voters'])
+
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+candidate_votes = 3345
+total_votes = 23123
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+print(message_to_candidate)
